5203069076da14e48085f5facbb39715d22aa720
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2021-09-28 16:07:56 +0200

    adding split_label function ...

--- pmacct_ptagmap_p1.c
+++ pmacct_ptagmap_p1.c
@@ -33,37 +33,37 @@ main(void)
   //printf("%s\n", tkns[2]);
   //printf("%s\n", tkns[3]);
 
-  //j_str_tmp = json_string(other1);
-  //json_object_set_new(root_l0, "other1", j_str_tmp);
+  j_str_tmp = json_string(other1);
+  json_object_set_new(root_l0, "other1", j_str_tmp);
 
-  //j_str_tmp = json_string(other2);
-  //json_object_set_new(root_l0, "other2", j_str_tmp);
+  j_str_tmp = json_string(other2);
+  json_object_set_new(root_l0, "other2", j_str_tmp);
 
-  //j_str_tmp = json_string(other3);
-  //json_object_set_new(root_l0, "other3", j_str_tmp);
+  j_str_tmp = json_string(other3);
+  json_object_set_new(root_l0, "other3", j_str_tmp);
 
-  //j_str_tmp = json_string(tkns[1]);
-  //json_object_set_new(root_l1, tkns[0], j_str_tmp);
+  j_str_tmp = json_string(tkns[1]);
+  json_object_set_new(root_l1, tkns[0], j_str_tmp);
 
-  //j_str_tmp = json_string(tkns[3]);
-  //json_object_set_new(root_l1, tkns[2], j_str_tmp);
+  j_str_tmp = json_string(tkns[3]);
+  json_object_set_new(root_l1, tkns[2], j_str_tmp);
 
-  //char *j_dump_l0  = json_dumps(root_l0, JSON_INDENT(2));
-  //printf("%s\n", j_dump_l0);
-  //free(j_dump_l0);
+  char *j_dump_l0  = json_dumps(root_l0, JSON_INDENT(2));
+  printf("%s\n", j_dump_l0);
+  free(j_dump_l0);
 
-  //json_object_set_new(root_l0, "label", root_l1);
+  json_object_set_new(root_l0, "label", root_l1);
 
-  //char *j_dump_l1  = json_dumps(root_l1, JSON_INDENT(2));
-  //printf("%s\n", j_dump_l1);
-  //free(j_dump_l1);
+  char *j_dump_l1  = json_dumps(root_l1, JSON_INDENT(2));
+  printf("%s\n", j_dump_l1);
+  free(j_dump_l1);
 
-  //char *j_dump_final  = json_dumps(root_l0, JSON_INDENT(2));
-  //printf("%s\n", j_dump_final);
-  //free(j_dump_final);
+  char *j_dump_final  = json_dumps(root_l0, JSON_INDENT(2));
+  printf("%s\n", j_dump_final);
+  free(j_dump_final);
 
-  //json_decref(root_l0);
-  //json_decref(root_l1);
+  json_decref(root_l0);
+  json_decref(root_l1);
 
   return 0;
 }
