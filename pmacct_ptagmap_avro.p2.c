5b9006a14c0b0b36a2fa370840a6285984d6b17b
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2021-10-12 18:36:59 +0200

    starting the modularization ...

--- pmacct_ptagmap_avro.p2.c
+++ pmacct_ptagmap_avro.p2.c
@@ -7,7 +7,6 @@
 #include <avro.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <jansson.h>
 
 
 /*
@@ -22,14 +21,27 @@
  * ---------------------------------------------------------------------------------
  * 1. add value string to the map -> avro_value_t v_type_string
  * 2. add value map to the record -> avro_value_t v_type_map
+ *
+ * code simplification - main blocks:
+ *
+ * - schema init/dump
+ * - data load
+ * - data print
  */
 
 
 /* Global Variables */
-char LABELS[] = "node_id_key1,node_id_value1,platform_id_key1,platform_id_value1,"
-                "node_id_key2,node_id_value2,platform_id_key2,platform_id_value2";
+char LABELS0[] = "node_id_key1,node_id_value1,platform_id_key1,platform_id_value1,"
+                 "node_id_key2,node_id_value2,platform_id_key2,platform_id_value2";
+char LABELS1[] = "node_id_key1,node_id_value1,platform_id_key1,platform_id_value1,"
+                 "node_id_key2,node_id_value2,platform_id_key2,platform_id_value2";
 const int MAX_TOCKENS = 256; //Max amount of tokens per string: 128 Labels
 const char *DELIM = ",";
+//
+// --- AVRO global variables ---
+//
+avro_schema_t sc_type_record, sc_type_map, sc_type_string;
+avro_value_t v_type_string, v_type_map, v_type_record;
 
 /* Data structures */
 struct label {
@@ -44,41 +56,39 @@ struct label *append_label(struct label *, struct label *);
 struct label *labels_to_linked_list(char *);
 void print_labels(struct label *);
 void free_labels(struct label *);
-json_t *labels_to_json_obj(struct label *);
+//
+// --- AVRO prototypes ---
+//
 
 
 int
 main(void)
 {
-  struct label *start = labels_to_linked_list(LABELS);
-  print_labels(start);
+  struct label *start_w = labels_to_linked_list(LABELS0);
+  struct label *start_r = labels_to_linked_list(LABELS1);
+  struct label *ptr_w, *ptr_r;
+  printf("start_w:\n");
+  print_labels(start_w);
+  printf("\nstart_r:\n");
+  print_labels(start_r);
+  printf("\n---\n");
 
-  json_t *root_l0 = json_object();
-  json_t *root_l1 = labels_to_json_obj(start);
-  json_t *j_str_tmp = NULL;
-
-  json_object_set_new(root_l0, "label", root_l1);
-
-  char *j_dump_final  = json_dumps(root_l0, JSON_INDENT(2));
-  printf("%s\n", j_dump_final);
-  free(j_dump_final);
-
-  json_decref(root_l0);
-  free_labels(start);
-
-  avro_value_t v_type_string;
-  avro_value_t v_type_map;
-  avro_value_t v_type_record;
-
-  avro_schema_t sc_type_string = avro_schema_string();
-  avro_schema_t sc_type_map = avro_schema_map(sc_type_string);
-  avro_schema_t sc_type_record = avro_schema_record("acct_data", NULL);
-
+  sc_type_string = avro_schema_string();
+  sc_type_map = avro_schema_map(sc_type_string);
+  sc_type_record = avro_schema_record("acct_data", NULL);
   avro_schema_record_field_append(sc_type_record, "label", sc_type_map);
+
+  FILE *avro_schema_fp = NULL;
+  avro_writer_t avro_schema_writer = NULL;
+  avro_schema_fp = fopen("avro_schema.dump", "w");
+  avro_schema_writer = avro_writer_file(avro_schema_fp);
+  avro_schema_to_json(sc_type_record, avro_schema_writer);
+  fclose(avro_schema_fp);
 
   avro_value_iface_t *if_type_record = avro_generic_class_from_schema(sc_type_record);
   avro_value_iface_t *if_type_map = avro_generic_class_from_schema(sc_type_map);
   avro_value_iface_t *if_type_string = avro_generic_class_from_schema(sc_type_string);
+
   avro_generic_value_new(if_type_record, &v_type_record);
   avro_generic_value_new(if_type_map, &v_type_map);
   avro_generic_value_new(if_type_string, &v_type_string);
@@ -87,35 +97,30 @@ main(void)
   const char *dbname = "avro_record.db";
 
   remove(dbname);
-  avro_file_writer_create(dbname, sc_type_record, &db_w);
+  int rval_w = avro_file_writer_create(dbname, sc_type_record, &db_w);
+  if (rval_w)
+  {
+    fprintf(stderr, "Error: %s\n", avro_strerror());
+    return -1;
+	}
 
   size_t  map_size;
   avro_value_get_size(&v_type_map, &map_size);
   printf("before: %u\n", map_size);
 
-  avro_value_get_by_name(&v_type_record, "label", &v_type_map, NULL);
-  avro_value_add(&v_type_map, "sc_map_key", &v_type_string, NULL, NULL);
-  avro_value_set_string(&v_type_string, "sc_map_value");
-  avro_file_writer_append_value(db_w, &v_type_record);
-
-  //avro_value_reset(&v_type_string);
-  //avro_value_reset(&v_type_record);
-
-  avro_value_get_by_name(&v_type_record, "label", &v_type_map, NULL);
-  avro_value_add(&v_type_map, "sc_map_key1", &v_type_string, NULL, NULL);
-  avro_value_set_string(&v_type_string, "sc_map_value1");
-  avro_file_writer_append_value(db_w, &v_type_record);
-
-  //avro_value_reset(&v_type_string);
-  //avro_value_reset(&v_type_record);
-
-  avro_value_get_by_name(&v_type_record, "label", &v_type_map, NULL);
-  avro_value_add(&v_type_map, "sc_map_key2", &v_type_string, NULL, NULL);
-  avro_value_set_string(&v_type_string, "sc_map_value2");
-  avro_file_writer_append_value(db_w, &v_type_record);
-
-  //avro_value_reset(&v_type_string);
-  //avro_value_reset(&v_type_record);
+  ptr_w = start_w;
+  while (ptr_w != NULL)
+  {
+    if (avro_value_get_by_name(&v_type_record, "label", &v_type_map, NULL) == 0)
+    {
+      if (avro_value_add(&v_type_map, ptr_w->key, &v_type_string, NULL, NULL) == 0)
+      {
+        avro_value_set_string(&v_type_string, ptr_w->value);
+        avro_file_writer_append_value(db_w, &v_type_record);
+      }
+    }
+    ptr_w = ptr_w->next;
+  }
 
   avro_file_writer_flush(db_w);
   avro_file_writer_close(db_w);
@@ -123,54 +128,43 @@ main(void)
   avro_value_get_size(&v_type_map, &map_size);
   printf("after: %u\n", map_size);
 
-  FILE *avro_schema_fp = NULL;
-  avro_writer_t avro_schema_writer = NULL;
-
-  avro_schema_fp = fopen("avro_schema.dump", "w");
-  avro_schema_writer = avro_writer_file(avro_schema_fp);
-  avro_schema_to_json(sc_type_record, avro_schema_writer);
-  fclose(avro_schema_fp);
-
-  //--------------------------------------------
-
   avro_file_reader_t db_r;
-  avro_file_reader_read_value(db_r, &v_type_record);
-  int rval = avro_file_reader(dbname, &db_r);
-  size_t value_size;
-  const char *p = NULL;
-
-  if (rval)
+  int rval_r = avro_file_reader(dbname, &db_r);
+  if (rval_r)
   {
     fprintf(stderr, "Error: %s\n", avro_strerror());
     return -1;
 	}
 
-  avro_value_get_by_name(&v_type_record, "label", &v_type_map, NULL);
-  avro_value_get_by_name(&v_type_map, "sc_map_key", &v_type_string, NULL);
-  avro_value_get_string(&v_type_string, &p, &value_size);
-  fprintf(stdout, "%u\n", value_size);
-  fprintf(stdout, "%s\n", p);
+  avro_file_reader_read_value(db_r, &v_type_record);
+  size_t value_size;
+  const char *p = NULL;
 
-  avro_value_get_by_name(&v_type_record, "label", &v_type_map, NULL);
-  avro_value_get_by_name(&v_type_map, "sc_map_key1", &v_type_string, NULL);
-  avro_value_get_string(&v_type_string, &p, &value_size);
-  fprintf(stdout, "%u\n", value_size);
-  fprintf(stdout, "%s\n", p);
-
-  avro_value_get_by_name(&v_type_record, "label", &v_type_map, NULL);
-  avro_value_get_by_name(&v_type_map, "sc_map_key2", &v_type_string, NULL);
-  avro_value_get_string(&v_type_string, &p, &value_size);
-  fprintf(stdout, "%u\n", value_size);
-  fprintf(stdout, "%s\n", p);
+  ptr_r = start_r;
+  while (ptr_r != NULL)
+  {
+    if (avro_value_get_by_name(&v_type_record, "label", &v_type_map, NULL) == 0)
+    {
+      if (avro_value_get_by_name(&v_type_map, ptr_r->key, &v_type_string, NULL) == 0)
+      {
+        avro_value_get_string(&v_type_string, &p, &value_size);
+      }
+    }
+    //fprintf(stdout, "%u\n", value_size);
+    fprintf(stdout, "%s:  %s\n", ptr_r->key, p);
+    ptr_r = ptr_r->next;
+  }
 
   avro_file_reader_close(db_r);
 
+  free_labels(start_w);
+  free_labels(start_r);
   avro_value_iface_decref(if_type_record); //no need to decref the associated value
   avro_value_iface_decref(if_type_map); //no need to decref the associated value
   avro_value_iface_decref(if_type_string); //no need to decref the associated value
+  avro_schema_decref(sc_type_record);
   avro_schema_decref(sc_type_map);
   avro_schema_decref(sc_type_string);
-  avro_schema_decref(sc_type_record);
   //avro_value_decref(&v_type_record);
   //avro_value_decref(&v_type_map);
   //avro_value_decref(&v_type_string);
@@ -203,7 +197,7 @@ print_labels(struct label *start)
   struct label *ptr;
   ptr = start;
   while(ptr != NULL) {
-    printf("%s -> %s\n", ptr->key, ptr->value);
+    printf("%s: %s\n", ptr->key, ptr->value);
     ptr = ptr->next;
   }
 }
@@ -218,23 +212,6 @@ free_labels(struct label *start)
     free(ptr);
     ptr = tmp;
   }
-}
-
-json_t *
-labels_to_json_obj(struct label *start)
-{
-  struct label *ptr;
-  ptr = start;
-  json_t *root = json_object();
-  json_t *j_str_tmp = NULL;
-
-  while(ptr != NULL) {
-    j_str_tmp = json_string(ptr->value);
-    json_object_set_new(root, ptr->key, j_str_tmp);
-    ptr = ptr->next;
-  }
-
-  return root;
 }
 
 struct label *
